0576b85fbdea6fb4ead7dc266a6bb78168352684
nlegouy <nicolas.legouy@insa-rouen.fr>
2017-03-17 10:31:59 +0100

    une modification.

---
+++ ProgramOne/src/ProgramOne.cpp
@@ -0,0 +1,16 @@
+//============================================================================
+// Name        : ProgramOne.cpp
+// Author      : Nicolas
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	cout << "première modification";
+	return 0;
+}
